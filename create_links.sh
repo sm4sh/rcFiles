6232c6686bcc0bd9098a8de80e0694f711376b64
Bastian Böhne <4398967+sm4sh@users.noreply.github.com>
2018-02-26 10:59:56 +0100

    working link creation

--- create_links.sh
+++ create_links.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-rc="~/src/rcFiles/"
-cd ~
-ln -s $(rc).cvimrc .
-ln -s $(rc).ideavimrc .
-ln -s $(rc).lsyncd .
-ln -s $(rc).lsyncd.exclude .
-ln -s $(rc).vimrc .
-ln -s $(rc).zsh_history .
-ln -s $(rc).zshrc .
+rc=$(pwd)
+ln -sf $rc/.cvimrc ~/.cvimrc
+ln -sf $rc/.ideavimrc ~/.ideavimrc
+ln -sf $rc/.lsyncd ~/.lsyncd
+ln -sf $rc/.lsyncd.exclude ~/.lsyncd.exclude
+ln -sf $rc/.vimrc ~/.vimrc
+ln -sf $rc/.zsh_history ~/.zsh_history
+ln -sf $rc/.zshrc ~/.zshrc
