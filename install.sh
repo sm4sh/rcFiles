3302cbbc3a0e5921a09516bb0792590469898ec5
Bastian Böhne <4398967+sm4sh@users.noreply.github.com>
2018-02-26 11:36:58 +0100

    added install script for various other things

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+mkdir ~/src
+git clone https://github.com/powerline/fonts.git ~/src/fonts
+sh ~/src/fonts/install.sh
+sh ~/src/rcFiles/create_links.sh
