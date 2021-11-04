700b7a87d58568f54a5c79e6bc6903439b9748bb
Bastian Boehne <bastian.boehne@avenga.com>
2021-11-04 10:20:17 +0100

    remove zsh history and swap file

--- create_links.sh
+++ create_links.sh
@@ -7,7 +7,6 @@ ln -sf $rc/.lsyncd.exclude ~/.lsyncd.exclude
 ln -sf $rc/.tmux.conf ~/.tmux.conf
 ln -sf $rc/tmux.sh ~/tmux.sh
 ln -sf $rc/.vimrc ~/.vimrc
-ln -sf $rc/.zsh_history ~/.zsh_history
 ln -sf $rc/.zshrc ~/.zshrc
 
 cd /usr/share/X11/xkb/symbols/
